4358b0932e27c2756b92e983f09d3cdfb66f5914
Biswajee <roy.biswajeet161@gmail.com>
2021-08-25 19:15:32 +0530

    Update logic and workflow

--- src/HelloWorld/index.spec.ts
+++ src/HelloWorld/index.spec.ts
@@ -1,13 +1,26 @@
 import chai, { expect } from 'chai';
-import * as tl from 'azure-pipelines-task-lib';
-import run from './index';
 import sinonChai from 'sinon-chai';
+import * as sinon from 'sinon';
+import run from './index';
 import chaiAsPromised from 'chai-as-promised';
 
 chai.use(sinonChai);
 chai.use(chaiAsPromised);
 
+// Sandbox for mocks
+let sandbox: sinon.SinonSandbox;
+
 describe('index', () => {
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+
+    sandbox.stub(console, 'log');
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
   it(`should call console.log() with expected parameter`, async() => {
     // Act
     await run();
--- src/HelloWorld/index.ts
+++ src/HelloWorld/index.ts
@@ -1,6 +1,6 @@
 import * as tl from 'azure-pipelines-task-lib';
 
-const run = async () => {
+const run = async (): Promise<void> => {
   try {
     // The simple line below prints hello world on pipeline
     console.log("Hello world!");
@@ -14,4 +14,8 @@ const run = async () => {
 
 export default run;
 
-run();
+// Do not execute the script if NODE_ENV is set to test
+// because we'll probably expect to run our test scripts
+if (process.env.NODE_ENV !== 'test') {
+  run().then();
+}
